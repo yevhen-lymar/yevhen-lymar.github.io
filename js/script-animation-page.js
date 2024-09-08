9d853e193e911d5db8931940cdb51c6f7e51ca37
Yevhen Lymar <lymar_evgeniy@ukr.net>
2024-09-08 14:57:50 +0300

    edit 08/09/2024-2 edit loader

--- js/script-animation-page.js
+++ js/script-animation-page.js
@@ -1,14 +1,31 @@
-// Отримуємо всі посилання на сторінці
-const links = document.querySelectorAll('a[href]:not([href^="#"]):not([href^="javascript:"])');
+document.addEventListener('DOMContentLoaded', function () {
+    // Плавне завантаження сторінки
+    document.body.classList.add('fade-in-active');
 
-// Додаємо обробник подій для кожного посилання
-links.forEach(function (link) {
-    link.addEventListener('click', function (event) {
-        event.preventDefault(); // Забороняємо негайний перехід
-        document.getElementById('loader').style.display = 'block'; // Показуємо лоадер
+    // Отримуємо всі посилання на сторінці
+    const links = document.querySelectorAll('a[href]:not([href^="#"]):not([href^="javascript:"])');
 
-        setTimeout(function () {
-            window.location.href = link.href; // Переходимо на іншу сторінку після затримки
-        }, 1000); // Можна налаштувати час затримки
+    // Лоадер і контент
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('contentAll');
+
+    // Додаємо обробник події для всіх посилань
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault(); // Зупиняємо звичайну дію посилання
+
+            // Прибираємо контент (fade-out)
+            content.classList.add('fade-out');
+
+            // Відображаємо лоадер після того, як контент зник
+            setTimeout(function () {
+                loader.style.display = 'flex';
+
+                // Перехід на нову сторінку з затримкою для лоадера
+                setTimeout(function () {
+                    window.location.href = link.href;
+                }, 1500); // Затримка для лоадера
+            }, 800); // Час для зникнення контенту
+        });
     });
 });
