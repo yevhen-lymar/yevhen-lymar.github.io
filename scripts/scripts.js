b62b811b98f4b0c77e5264c8795edb4a6ae119eb
Yevhen Lymar <lymar_evgeniy@ukr.net>
2024-11-20 22:38:20 +0200

    edit 20/11/24 22-38

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,14 +2,20 @@ const toggleLangBtn = document.getElementById("lang-toggle");
 const pageContent = document.getElementById("page-content");
 const loader = document.getElementById("loader");
 const scrollUpBtn = document.getElementById("up-scroll");
+const aboutBtn = document.getElementById("aboutBtn");
+const modal = document.getElementById("modal");
 
 let currentLang = "en";
 let translations = {};
 
+async function fetchLang() {
+  const response = await fetch("../language/translations.json");
+  return response.json();
+}
+
 async function loadTranslations() {
   try {
-    const response = await fetch("../language/translations.json");
-    translations = await response.json();
+    translations = await fetchLang();
     // console.log(translations)
     const savedLang = localStorage.getItem("language") || "en";
     updateLanguage(savedLang);
@@ -71,3 +77,47 @@ window.addEventListener("scroll", () => {
 });
 
 scrollUpBtn.addEventListener("click", scrollToUp);
+
+async function infoMeInModalWindow() {
+  const info = await fetchLang();
+  // console.log(info.en);
+
+  aboutBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const pageLang = document.documentElement.getAttribute("lang");
+    // console.log(info[pageLang]);
+
+    const title = info[pageLang].info_me_title;
+    const text = info[pageLang].info_me_text;
+
+    // console.log(title);
+    // console.log(text);
+
+    modal.innerHTML = `
+    <div class="modal-content">
+      <div class="modal-title-box">
+        <div class="close"><i class="fas fa-xmark"></i></div>
+      </div>
+      <div class="modal-info-box">
+        <h2>${title}</h2>
+        <div class="modal-text-box">${text}</div>
+      </div>
+    </div>  
+  `;
+    modal.style.display = "block";
+
+    modal.querySelector(".close").addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    // Close Modal Window
+    window.addEventListener("click", (event) => {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+}
+
+infoMeInModalWindow();
