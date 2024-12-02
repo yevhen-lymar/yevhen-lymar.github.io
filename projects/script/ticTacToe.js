2a1ee1d277b36ff4cbe534fed69f10fd98390321
Yevhen Lymar <lymar_evgeniy@ukr.net>
2024-12-02 19:51:06 +0200

    edit tic tac toe

--- projects/script/ticTacToe.js
+++ projects/script/ticTacToe.js
@@ -1,28 +1,17 @@
 const gameBoard = document.getElementById("container-board");
 const restartBtn = document.getElementById("restartBtn");
 
-let currentPlayer = "X";
-
-const cells = document.querySelectorAll(".cell");
-const gameState = Array(9).fill(null);
-
-const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
 for (let i = 0; i < 9; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
   cell.dataset.index = i;
   gameBoard.appendChild(cell);
 }
+
+let currentPlayer = "X";
+
+const cells = document.querySelectorAll(".cell");
+const gameState = Array(9).fill(null);
 
 function handleCellClick(event) {
   const cell = event.target;
@@ -51,6 +40,16 @@ cells.forEach((cell) => {
 });
 
 function checkWinner() {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   for (const combination of winningCombinations) {
     const [a, b, c] = combination;
     if (
